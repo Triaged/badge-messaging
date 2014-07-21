2a6a27b60a51fdf3eb65371253929f4c1bffd03c
Charlie White <charlie@triaged.co>
2014-07-21 16:07:09 -0400

    return message thread

--- app/faye/message_controller.rb
+++ app/faye/message_controller.rb
@@ -11,13 +11,9 @@ class MessageController < FayeRails::Controller
     monitor :publish do
       Emlogger.instance.log "Client #{client_id} published #{data.inspect} to #{channel}."
       
-      Emlogger.instance.log data["guid"]
-
       guid = data["guid"]
       published_message = Hashie::Mash.new(data["message"])
       
-      Emlogger.instance.log guid
-
       thread = MessageThread.first
       message = thread.messages.create(
         author_id: published_message.author_id,
@@ -25,16 +21,9 @@ class MessageController < FayeRails::Controller
         timestamp: published_message.timestamp
       )
 
-      Emlogger.instance.log message.inspect
-      Emlogger.instance.log message.as_json
-
-      response = {message: message.attributes, guid: guid}
-
+      response = {message_thread: thread.to_json, guid: guid}
       Emlogger.instance.log response
-    
-       Emlogger.instance.log "about to publish"  
-       MessageController.publish("/users", response)
+      MessageController.publish("/users", response)
     end
   end
-
 end
