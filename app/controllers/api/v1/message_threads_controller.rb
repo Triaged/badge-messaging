ad5afe743f0b1d4a7999fff02c717aa69cdc2762
Charlie White <charlie@triaged.co>
2014-07-21 17:14:43 -0400

    logging

--- app/controllers/api/v1/message_threads_controller.rb
+++ app/controllers/api/v1/message_threads_controller.rb
@@ -7,6 +7,8 @@ class Api::V1::MessageThreadsController < ApplicationController
 	end
 
 	def create
+		Rails.logger.info params[:message_thread]
+		Rails.logger.info message_thread_params
 		@message_thread = MessageThread.create(message_thread_params)
 		respond_with @message_thread.to_json, location: api_v1_message_thread_path(@message_thread)
 	end
