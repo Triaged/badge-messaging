70109de3f5bd209dbe64b32240944c78bdef2556
Charlie White <charlie@triaged.co>
2014-07-21 16:03:38 -0400

    moped

--- app/models/message_thread.rb
+++ app/models/message_thread.rb
@@ -4,12 +4,5 @@ class MessageThread
   has_and_belongs_to_many :users
 	embeds_many :messages
 
-	def as_json
-		{
-			id: self.id.to_s,
-			users: self.users,
-			messages: self.messages.as_json
-		}
-
-	end
+	
 end
