174e399b89747152806e04d8e621b814cdd527f7
Charlie White <charlie@triaged.co>
2014-07-22 13:28:02 -0400

    message history

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -11,7 +11,7 @@ class ApiController < ApplicationController
   end
 
   def authenticate_from_token!
-  	user_id = request.headers["User-Id"].presence
+  	user_id = request.headers["HTTP_USER_ID"].presence
     auth_token = request.headers["HTTP_AUTHORIZATION"].presence
     unless auth_token && user_id && (User.find(user_id).valid_auth_token?(auth_token))
       render :json => { :errors => ['You must be signed in to access this page'] }, :status => 401
@@ -25,5 +25,5 @@ class ApiController < ApplicationController
     user_id = request.headers["User-Id"].presence
     User.find(user_id)
   end
-  
+
 end
