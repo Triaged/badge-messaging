1e00ba4d69a7536b19d22fd7d3f705a0c1c98dd3
Charlie White <charlie@triaged.co>
2014-07-28 11:40:41 -0400

    publish heartbeat

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem 'bugsnag'
 gem 'httparty'
 gem 'figaro'
 
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/faye/user_channel_controller.rb
+++ app/faye/user_channel_controller.rb
@@ -22,21 +22,23 @@ class UserChannelController < FayeRails::Controller
   channel '/users/heartbeat/*' do
     monitor :subscribe do
       Emlogger.instance.log "User #{client_id} subscribed to #{channel}."
-      user = UserChannelController.user(channel)
-      user.subscribed
-      #PendingMessagesController.new(user).publish_pending_threads
-      Emlogger.instance.log "#{user.id} subscriptions: #{user.subscriptions}"
     end
     monitor :unsubscribe do
       Emlogger.instance.log "User #{client_id} unsubscribed from #{channel}."
     end
     monitor :publish do
+      HeartbeatService.received_heartbeat(UserChannelController.user_id(channel))
       Emlogger.instance.log "Client #{client_id} published #{data.inspect} to #{channel}."
     end
   end
 
   def self.user(channel)
-    user_id = /.*\/(.*)/.match(channel)[1]
+    user_id = UserChannelController.user_id(channel)
     return User.find_or_create_by(user_id: user_id)
   end
+
+  def self.user_id (channel)
+    return /.*\/(.*)/.match(channel)[1]
+  end
+
 end
