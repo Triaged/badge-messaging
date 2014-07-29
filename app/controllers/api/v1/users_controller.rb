b1a30d708e2fe9886f8f76b0b32d41c648a7b969
Charlie White <charlie@triaged.co>
2014-07-29 18:03:32 -0400

    removed timestamp check on message threadds for history

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UsersController < ApiController
 
 	def messages
 		timestamp = params[:timestamp].to_f || 0
-		@threads = current_user.pending_message_threads_since(timestamp)
+		@threads = current_user.message_threads
 		render json: @threads, each_serializer: MessageThreadSerializer, root: false, options: { timestamp: timestamp }
 	end
 end
