2d93469a2c6d40a12153a1ee1f01807b114023da
Charlie White <charlie@triaged.co>
2014-07-29 18:29:12 -0400

    timestamps

--- app/serializers/message_thread_serializer.rb
+++ app/serializers/message_thread_serializer.rb
@@ -1,10 +1,9 @@
 class MessageThreadSerializer < ActiveModel::Serializer
-  attributes :id, :user_ids, :u_at
+  attributes :id, :user_ids, :u_at, :messages
 
-  has_many :messages
+  
 
   def messages
-  	Rails.logger.info @options.inspect
   	return @options[:messages] if @options[:messages]
   	return object.messages.where(:c_at.gt => @options[:timestamp]) if @options[:timestamp]
   	return object.messages
