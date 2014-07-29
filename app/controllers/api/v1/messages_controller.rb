643d70b77b75dc356b20853e3920e781e41313f8
Charlie White <charlie@triaged.co>
2014-07-29 15:10:03 -0400

    message test harness

---
+++ app/controllers/api/v1/messages_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::MessagesController < ApplicationController
+	respond_to :json
+	protect_from_forgery with: :null_session
+
+	def create
+		@message = MessageThread.in(user_ids: ["6"]).first.messages.new(message_params)
+		respond_with @message
+	end
+
+	def message_thread_params
+		params[:message].permit(:author_id, :body, :timestamp)
+	end
+
+
+end
