934724ea08e4e3c51841309f4cd0437f533877fb
Charlie White <charlie@triaged.co>
2014-07-29 14:20:08 -0400

    histroy

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::UsersController < ApiController
 
 	def messages
 		timestamp = params[:timestamp] || 0
-		respond_with current_user.pending_message_threads_since(timestamp), timestamp: timestamp.to_f
+		@threads = current_user.pending_message_threads_since(timestamp), timestamp: timestamp.to_f
+		render json: @threads, each_serializer: MessageThreadSerializer
 	end
 end
