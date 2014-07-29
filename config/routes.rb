81bef1b5e343d2e27acc40a558b2509c9a94b1d5
Charlie White <charlie@triaged.co>
2014-07-29 15:12:16 -0400

    message test harness

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      resources :messages
       resources :message_threads
       resource :user do
         get 'messages', as: :member
