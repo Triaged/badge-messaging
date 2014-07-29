a47347ebdbd9300da8949d17e52c2f1f91a5967e
Charlie White <charlie@triaged.co>
2014-07-29 18:22:18 -0400

    timestamps

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,6 +3,6 @@ class Api::V1::UsersController < ApiController
 	def messages
 		timestamp = params[:timestamp].to_f || 0
 		@threads = current_user.message_threads_since(timestamp)
-		render json: @threads, each_serializer: MessageThreadSerializer, root: false, timestamp: timestamp
+		render json: @threads, timestamp: timestamp, each_serializer: MessageThreadSerializer, root: false
 	end
 end
