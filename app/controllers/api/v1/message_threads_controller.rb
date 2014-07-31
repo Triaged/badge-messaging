3c33482f01bc4e0956e89ecdeed4b618fb5baa03
Charlie White <charlie@triaged.co>
2014-07-31 09:58:04 -0400

    log create headers

--- app/controllers/api/v1/message_threads_controller.rb
+++ app/controllers/api/v1/message_threads_controller.rb
@@ -7,12 +7,12 @@ class Api::V1::MessageThreadsController < ApiController
 	end
 
 	def create
-		logger.warn "*** BEGIN RAW REQUEST HEADERS ***"
+		puts "*** BEGIN RAW REQUEST HEADERS ***"
 		self.request.env.each do |header|
-  		logger.warn "HEADER KEY: #{header[0]}"
-  		logger.warn "HEADER VAL: #{header[1]}"
+  		puts "HEADER KEY: #{header[0]}"
+  		puts "HEADER VAL: #{header[1]}"
 		end
-		logger.warn "*** END RAW REQUEST HEADERS ***"
+		puts "*** END RAW REQUEST HEADERS ***"
 
 
 		recipients = message_thread_params[:user_ids]
